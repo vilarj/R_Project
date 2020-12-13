f192ef87895ba5df7b8b31b800bcee57bb71bc32
Vilarj <lamaravillasan@outlook.com>
2020-12-13 13:18:18 -0400

    R.r
    Progress in solving for a, b, c, d, e, and f

--- R.r
+++ R.r
@@ -2,7 +2,6 @@
 library(readxl)
 Computer_Stats <- read_excel("C:/Users/Vilarj/Desktop/Computer_Stats.xlsx")
 
-
 View(Computer_Stats)
 class(Computer_Stats)
 
@@ -20,23 +19,40 @@ Daily_PP <- as.numeric(Computer_Stats$Daily.PP)
 View(Computer_Stats)
 
 # Creating a mode function
-getMode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
+getMode <- function(vector) {
+  uniqv <- unique(vector)
+  uniqv[which.max(tabulate(match(vector, uniqv)))]
 }
 
-# Daily AC 2017
+# (A) Daily AC 2017
 mean(Daily_AC)
 median(Daily_AC)
 getMode(Daily_AC)
 sd(Daily_AC)
 
-# Daily PP 2017
+
+# (A) Daily PP 2017
 mean(Daily_PP)
 median(Daily_PP)
 getMode(Daily_PP)
 sd(Daily_PP)
 
+# (B) Histogram of Daily AC and Daily PP
+hist(Daily_AC, main = "Histogram AC", col = "brown", border = "yellow")
+hist(Daily_PP, main = "Histogram PP", col = "yellow", border = "brown")
+
+# (C) Box plot of Daily_AC and Daily_PP 
+boxplot(Daily_AC, main = "Daily AC", xlab = "M_AC <LEFT ---- RIGHT> Daily_AC", col = "green", border = "red")
+boxplot(Daily_PP, main = "Daily PP", xlab = "M_AC <LEFT ---- RIGHT> Daily_AC", col = "gold", border = "brown")
+
+# (D) Normal Distribution of Daily AC
+pnorm(35.4, mean(Daily_AC), sd(Daily_AC))
+
+# (E) Normal Distribution of Daily PP
+pnorm(35.4, mean(Daily_PP), sd(Daily_PP))
+
+# (F) 
+pnorm(330, mean(Daily_PP), sd(Daily_PP))
 
 
 
